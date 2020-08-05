10353587fc96e313e96066e15bb5e22065d3439e
BuildTools <unconfigured@null.spigotmc.org>
2020-08-05 11:49:16 -0400

    turned into a command-line tool

--- skephuts.c
+++ skephuts.c
@@ -1,24 +1,80 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <errno.h>
 #include "cubiomes/finders.h"
 
+#define DIST_FLAG (1 << 0)
+#define ORIGIN_DIST_FLAG (1 << 1)
+
 int count = 0;
+unsigned int flags = 0;
 
 void possible(Pos pos1, Pos pos2) {
 	//printf("(%d, %d)\n", pos1.x, pos1.z);
 	double dist = sqrt((pos2.x - pos1.x)*(pos2.x - pos1.x) + (pos2.z - pos1.z)*(pos2.z - pos1.z));
+	
 	if (dist < 256) {
-		double originDist = sqrt((((long)pos2.x)*pos2.x) + (((long)pos2.z)*pos2.z));
-		printf("(%d, %d), (%d, %d), %lf, %lf\n", pos1.x, pos1.z, pos2.x, pos2.z, dist, originDist);
+		printf("(%d, %d), (%d, %d)", pos1.x, pos1.z, pos2.x, pos2.z);
+		if (flags & DIST_FLAG)
+			printf(", %lf", dist);
+		if (flags & ORIGIN_DIST_FLAG) {
+			double originDist = sqrt((((long)pos1.x)*pos1.x) + (((long)pos1.z)*pos1.z));
+			printf(", %lf", originDist);
+		}
+		printf("\n");
+
 		count++;
 	}
 }
 
-int main() {
-	initBiomes();
+int main(int argc, char** argv) {
+	long seed;
+	char seedSet = 0;
 
-	long seed = 8054221181584912980;
+	for (int i=1; i<argc; i++) {
+		if (argv[i][0] == '-') {
+			switch (argv[i][1]) { 
+			case 'h':
+				printf(
+				"Skephuts- Minecraft 1.16 Witch Hut finder\n"
+				"Note: String seeds are not yet supported, only numeric.\n"
+				"Usage: skephuts [options] <seed>\n"
+				"Valid options:\n"
+				"	-h		Prints this message.\n"
+				"   -d      Includes the distance from one witch hut to the other\n"
+				"   -o      Includes the distance from the first witch hut to the origin.\n");
+				return 0;
+			case 'd':
+				flags |= DIST_FLAG;
+				break;
+			case 'o':
+				flags |= ORIGIN_DIST_FLAG;
+				break;
+			default:
+				printf("Unknown flag '-%c'. Use 'skephuts -h' for usage.\n", argv[i][1]);
+				return 1;
+			}
+		} else if (seedSet == 0) {
+			seedSet = 1;
+			errno = 0;
+			seed = strtol(argv[i], NULL, 10);
+			if (errno) {
+				printf("An error occured while parsing the seed.\n");
+				return 1;
+			}
+		} else {
+			printf("Unknown parameter %s. Use 'skephuts -h' for usage.\n", argv[i]);
+			return 1;
+		}
+	}
+	if (seedSet == 0) {
+		printf("No seed given! Use 'skephuts -h' for usage.\n");
+		return 1;
+	}
+
+	initBiomes();
 	
 	clock_t start_time = clock();
 	LayerStack g = setupGenerator(MC_1_16);
